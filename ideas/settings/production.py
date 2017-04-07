505ce0a93a15c68dd97e15857c80e301a16e10af
Yeimer Gabriel Molina Ramirez <yeimer.molina@gmail.com>
2017-04-07 11:59:32 -0400

    Minor update

--- ideas/settings/production.py
+++ ideas/settings/production.py
@@ -82,7 +82,7 @@ DATABASES = {
 
 import dj_database_url
 
-db_from_env = dj_database_url.config()
+db_from_env = dj_database_url.config(conn_max_age=500)
 DATABASES['default'].update(db_from_env)
 
 # Password validation
