cb53d245ce35f1030346a26dc71c442dfddb5a81
Yeimer Gabriel Molina Ramirez <yeimer.molina@gmail.com>
2017-04-10 14:01:03 -0400

    Add pages app

--- pages/views.py
+++ pages/views.py
@@ -1,17 +1,30 @@
+from django.contrib.messages.views import SuccessMessageMixin
 from django.shortcuts import render
 from django.views.generic import View, FormView, CreateView
 
 from newsletter.forms import JoinForm
+
+from .models import Page
+
 
 # class HomeView(View):
 # 	def get(self, request, *args, **kwargs):
 # 		return render(request, "pages/home.html", {})
 
 
-class HomeView(CreateView):
+class HomeView(SuccessMessageMixin, CreateView):
 	template_name = 'pages/home.html'
 	form_class = JoinForm
 	success_url = '/'
+
+	def get_context_data(self, *args, **kwargs):
+		context = super(HomeView,self).get_context_data(*args, **kwargs)
+		context['page_obj'] = Page.objects.all().order_by("?").first()
+		return context
+	
+	def get_success_message(self, cleaned_data):
+		print(cleaned_data)
+		return "Thanks for joining!"
 
 	# def form_valid(self, form):
 	# 	email = form.cleaned_data.get("email")
